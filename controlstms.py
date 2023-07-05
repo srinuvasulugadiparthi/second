65dcaf66f1e171435252894bd0e524b3ad975ff1
srinivasulu <srinuvasulugadiparthi@gmail.com>
2023-07-05 16:23:56 +0530

    the first commit

---
+++ controlstms.py
@@ -0,0 +1,11 @@
+n=int(input("enter your marks:"))
+if n>90:
+    print("grade A:")
+elif n>70:
+    print("grade B :")
+elif n>60:
+    print("grade c:")
+elif n>45:
+    print("grade D:")
+else:
+    print("grade fail:")
---
+++ loops.py
@@ -0,0 +1,20 @@
+# for i in range(10):
+#     print(i)
+
+#### even numbers
+# for i in range(21):
+#     if i%2==0:
+#         print(i)
+
+### odd numbers
+# for i in range(21):
+  # if i%2==1:
+      # print(i)
+# count=0
+# while count<5:
+#     print("count:", count)
+#     count +=1
+# for i in range(3):
+#     for j in range(2):
+#         print(f"oter loop iteration: {i}, inner loop iteration:{j}")
+    
