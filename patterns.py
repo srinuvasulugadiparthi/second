29eddb436f051a959758f0d219c0527fca9a4f4b
srinuvasulugadiparthi <138544585+srinuvasulugadiparthi@users.noreply.github.com>
2023-07-10 09:21:44 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,128 @@
+#x=int(input("enter a number"))
+#z=x
+#for i in range(x+1):
+ #   y=z
+  #  while(y>0):
+   #     print("a",end=" ")
+    #    y-=1
+    #print()
+    #z-=1
+
+    
+# x=int(input("enter a number"))
+# for i in range(x+1):
+#     y=i
+#     while(y>0):
+#         print("a",end=" " )
+#         y=y-1
+#     print()
+
+# x=int(input("enter a number"))
+# for i in range(x+1):
+#     y=i
+#     while(y>0):
+#         print("*",end=" " )
+#         y=y-1
+#     print()
+
+# x=int(input("enter a number"))
+# z=x
+# for i in range(x+1):
+#     y=z
+#     while(y>0):
+#         print("*",end=" " )
+#         y=y-1
+#     print()
+#     z=z-1
+
+# x=int(input("enter a number"))
+# for i in range(x+1):
+#     y=i
+#     x=1
+#     while(x<=y):
+#         print(x,end=" " )
+#         x=x+1
+#     print()
+# x=int(input("enter a number"))
+# for i in range(x+1):
+#     y=i
+#     while(y>0):
+#         print(y,end=" " )
+#         y=y-1
+#     print()
+# x=int(input("enter a number"))
+# for i in range(x+1):
+#     y=x
+#     while(y>0):
+#         print("a",end=" " )
+#         y=y-1
+#     print()
+# x=int(input("enter a number"))
+# for i in range(x+1):
+#     y=x
+#     while(y>0):
+#         print("*",end=" " )
+#         y=y-1
+#     print()
+# x=int(input("enter a number"))
+# for i in range(x):
+#     y=x
+#     s=1
+#     while(s<=y):
+#         print(s,end=" " )
+#         s=s+1
+#     print()
+# x=int(input("enter a number"))
+# for i in range(x):
+#     y=x
+#     while(y>0):
+#         print(i+1,end=" " )
+#         y=y-1
+#     print()
+
+# x=int(input("enter a even number"))
+# y=2
+# r=x
+# for i in range(x,0,-2):
+
+#     z=int((r/2)-1)
+#     while(z):
+#         print(" ",end=" ")
+#         z=z-1
+#     for j in range(y):
+#         print("a" ,end=" ")
+#     print()
+#     y=y+2
+#     r=r-2
+
+# x=int(input("enter a even number"))
+# y=2
+# r=x
+# for i in range(x,0,-2):
+
+#     z=int((r/2)-1)
+#     while(z):
+#         print(" ",end=" ")
+#         z=z-1
+#     for j in range(y):
+#         print("*" ,end=" ")
+#     print()
+#     y=y+2
+#     r=r-2
+
+x=int(input("enter a even number"))
+y=2
+r=x
+for i in range(x,0,-2):
+    s=1
+    z=int((r/2)-1)
+    while(z):
+        print(" ",end=" ")
+        z=z-1
+    for j in range(y):
+        print(s ,end=" ")
+        s=s+1
+    print()
+    y=y+2
+    r=r-2
+    
---
+++ sliceing.py
@@ -0,0 +1,77 @@
+#s=("welcome to python")  
+#slice_string=s[3:-6]
+#print(slice_string)
+
+
+#s="srinu ram sri"
+#x=s[0:7]
+#print(x)
+#s="srinu ram sri"
+#y=s[-1:-8:-1]
+#print(y) 
+#s="srinu ram sri"
+#z=s[-7:-10:-2]
+#print(z)
+#s="srinu ram sri"
+#a=s[7:12:4]
+#print(a)
+#s="srinu ram sri"
+#b=s[-4:-1:-1]
+#print(b)
+#s="srinu ram sri"
+#c=s[-7:-11:-1]
+#print(c)
+#s="srinu ram sri"
+#d=s[-9:9]
+#print(d)
+#s="srinu ram sri"
+#e=s[-7:-2]
+#print(e)
+#f="collection math"
+#x=f[14:-14:-1]
+#print(x)
+#f="collection math"
+#x=f[-8:]
+#print(x)
+#f="collection math"
+#x=f[-8: :-1]
+#print(x)
+#f="collection math"
+#x=f[ :-8:-1]
+#print(x)
+#f="collection math"
+#x=f[ :-8]
+#print(x)
+#f="collection math"
+#x=f[6:0]
+#print(x)
+#f="collection math"
+#x=f[14:-14]
+#print(x)
+#f="collection math"
+#x=f[14:-13:-1]
+#print(x)
+#f="collection math"
+#x=f[0:9]
+#print(x)
+#f="collection math"
+#x=f[5:-14:-1]
+#print(x)
+#f="collection math"
+#x=f[2:-15]
+#print(x)
+#f="collection math"
+#x=f[-11:13]
+#print(x)
+#f="collection math"
+#x=f[-11:11:-1]
+#print(x)
+#f="collection math"
+#x=f[-10:-14:-1]
+#print(x)
+#f="collection math"
+#x=f[-9:-4:-1]
+#print(x)
+
+
+
